e03f9556aff8cb5baa32a040b82ebf7acd472d86
Kotbi Mohamed <simojetta2@gmail.com>
2021-04-22 20:16:18 -0700

    commit 0.2

--- index.js
+++ index.js
@@ -10,20 +10,16 @@ async function test() {
     await  driver.get("https://www.google.com");
    await driver.get('https://www.soccerdonna.de/de/fc-bayern-muenchen/historische-kader/verein_2009.html');
   //await driver.findElement(By.className('message-component'))
-const element=By.xpath("//input[@class='message-component message-button no-children']");
-driver.wait(until.elementLocated(element));
-const whatelement=(await driver).findElement(element);
-  await driver.wait(until.elementIsVisible(whatelement),5000)
- .click()
+//const element=By.xpath("//button[@class='message-component message-button no-children']");
+const element=By.xpath("//button[text()='Zustimmen']");
+
+// driver.wait(until.elementLocated(element));
+// const whatelement=(await driver).findElement(element);
+//   await driver.wait(until.elementIsVisible(whatelement),5000)
+//  .click()
   //await driver.findElements(By.css('.message .message-component'))
  // .click()
- driver.findElements(element).then(function(elements){
-  elements.forEach(function (element) {
-      element.getText().then(function(text){
-          console.log(text);
-      });
-  });
-})
+ await (await driver).findElement(element).click()
   .then(()=>console.log("clicked succesfuly!!!"));
 }catch(err){console.log(err)}
 
